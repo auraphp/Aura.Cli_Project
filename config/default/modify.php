932b65f57976df7ae40074843c34fbcb62b18956
Paul M. Jones <pmjones88@gmail.com>
2013-11-21 20:38:52 -0600

    set dispatcher object param, and log to syslog instead of stdio

--- config/default/modify.php
+++ config/default/modify.php
@@ -4,9 +4,18 @@
  */
 
 // support objects
+$logger     = $di->get('logger');
 $dispatcher = $di->get('cli_dispatcher');
 $context    = $di->get('cli_context');
 $stdio      = $di->get('cli_stdio');
+
+// send logging to syslog instead of stdio
+$logger->pushHandler($di->newInstance('Monolog\Handler\SyslogHandler', array(
+    'ident' => 'aura-cli-project',
+)));
+
+// the command param name
+$dispatcher->setObjectParam('command');
 
 // add commands by name
 $dispatcher->setObject('hello', function ($name = 'world') use ($context, $stdio) {
