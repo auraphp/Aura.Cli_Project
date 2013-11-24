38a4743067688cc1ddb6804d739897edc48e1a0f
Paul M. Jones <pmjones88@gmail.com>
2013-11-24 15:13:17 -0600

    use standard logging location from project kernel

--- config/default/modify.php
+++ config/default/modify.php
@@ -4,20 +4,14 @@
  */
 
 // support objects
-$logger     = $di->get('logger');
 $dispatcher = $di->get('cli_dispatcher');
 $context    = $di->get('cli_context');
 $stdio      = $di->get('cli_stdio');
-
-// send logging to syslog instead of stdio
-$logger->pushHandler($di->newInstance('Monolog\Handler\SyslogHandler', array(
-    'ident' => 'aura-cli-project',
-)));
 
 // the command param name
 $dispatcher->setObjectParam('command');
 
 // add commands by name
-$dispatcher->setObject('hello', function ($name = 'world') use ($context, $stdio) {
+$dispatcher->setObject('hello', function ($name = 'World') use ($context, $stdio) {
     $stdio->outln("Hello {$name}!");
 });
