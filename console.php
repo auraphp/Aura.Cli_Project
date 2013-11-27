7100c6589c2cd3de2ef7779b34b8a215ce748dc7
Paul M. Jones <pmjones88@gmail.com>
2013-11-27 09:56:25 -0600

    extract bootstrap to kernel

--- console.php
+++ console.php
@@ -1,21 +1,13 @@
 <?php
-use Aura\Cli_Kernel\CliKernelFactory;
+/**
+ * 
+ * This file is part of Aura for PHP.
+ * 
+ * @package Aura.Web_Kernel
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ * 
+ */
 
-// the project base directory
-$base = __DIR__;
-
-// config mode
-$file = str_replace("/", DIRECTORY_SEPARATOR, "{$base}/config/_mode");
-$mode = trim(file_get_contents($file));
-if (! $mode) {
-    $mode = "default";
-}
-
-// autoloader
-$loader = require "{$base}/vendor/autoload.php";
-
-// create the project kernel, invoke it, and exit with its status code
-$factory = new CliKernelFactory;
-$kernel = $factory->newInstance($base, $mode, $loader);
-$status = (int) $kernel->__invoke();
-exit($status);
+// hand off to the kernel script
+require __DIR__ . '/vendor/aura/web-kernel/scripts/kernel.php';
