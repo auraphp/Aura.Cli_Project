7b15f4e3254a0f123dbde023ace85144070c79b2
Paul M. Jones <pmjones88@gmail.com>
2013-12-11 14:49:29 -0600

    split up config files

---
+++ config/default/define.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 
+ * This file is part of Aura for PHP.
+ * 
+ * @package Aura.Cli_Project
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ * 
+ * @var Aura\Di\Container $di The DI container.
+ * 
+ */
+
