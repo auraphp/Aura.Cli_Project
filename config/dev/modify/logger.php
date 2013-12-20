81a8e84432c70948ef502580fce7801456ab7f9f
Paul M. Jones <pmjones88@gmail.com>
2013-12-20 11:13:18 -0600

    no need for .cli in log file name

--- config/dev/modify/logger.php
+++ config/dev/modify/logger.php
@@ -17,7 +17,7 @@ $logger = $di->get('logger');
 
 // add a log handler
 $mode = $project->getMode();
-$file = $project->getTmpPath("log/{$mode}.cli.log");
+$file = $project->getTmpPath("log/{$mode}.log");
 $logger->pushHandler($di->newInstance('Monolog\Handler\StreamHandler', array(
     'stream' => $file,
 )));
