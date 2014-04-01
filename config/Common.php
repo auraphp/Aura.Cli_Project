326ae31db293688a81d549582d7dbd38067c8568
Paul M. Jones <pmjones88@gmail.com>
2014-04-01 12:48:31 -0500

    replace include-based config with class-based

---
+++ config/Common.php
@@ -0,0 +1,49 @@
+<?php
+namespace Aura\Cli_Project\_Config;
+
+use Aura\Di\Config;
+use Aura\Di\Container;
+
+class Common extends Config
+{
+    public function define(Container $di)
+    {
+        $di->set('logger', $di->newInstance('Monolog\Logger'));
+    }
+
+    public function modify(Container $di)
+    {
+        $this->modifyLogger($di);
+        $this->modifyCliDispatcher($di);
+    }
+
+    protected function modifyLogger(Container $di)
+    {
+        $project = $di->get('project');
+        $mode = $project->getMode();
+        $file = $project->getPath("tmp/log/{$mode}.log");
+
+        $logger = $di->get('logger');
+        $logger->pushHandler($di->newInstance(
+            'Monolog\Handler\StreamHandler',
+            array(
+                'stream' => $file,
+            )
+        ));
+    }
+
+    protected function modifyCliDispatcher(Container $di)
+    {
+        $context = $di->get('cli_context');
+        $stdio = $di->get('cli_stdio');
+        $logger = $di->get('logger');
+        $dispatcher = $di->get('cli_dispatcher');
+        $dispatcher->setObject(
+            'hello',
+            function ($name = 'World') use ($context, $stdio, $logger) {
+                $stdio->outln("Hello {$name}!");
+                $logger->debug("Said hello to '{$name}'");
+            }
+        );
+    }
+}
