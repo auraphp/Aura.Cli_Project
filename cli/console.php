b83e5b0f28d53c9178bfab3beb89a76b26917991
Paul M. Jones <pmjones88@gmail.com>
2014-04-02 13:28:31 -0500

    use newKernel()

--- cli/console.php
+++ cli/console.php
@@ -8,18 +8,11 @@
  * @license http://opensource.org/licenses/bsd-license.php BSD
  * 
  */
-
 $path = dirname(__DIR__);
 require "{$path}/vendor/autoload.php";
-require "{$path}/config/_env.php";
-
-$di = (new \Aura\Project_Kernel\Factory)->newContainer(
+$kernel = (new \Aura\Project_Kernel\Factory)->newKernel(
     $path,
-    $_ENV['AURA_CONFIG_MODE'],
-    "{$path}/composer.json",
-    "{$path}/vendor/composer/installed.json"
+    'Aura\Cli_Kernel\CliKernel'
 );
-
-$kernel = $di->newInstance('Aura\Cli_Kernel\CliKernel');
 $status = $kernel();
 exit($status);
